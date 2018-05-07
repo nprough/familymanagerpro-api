9b9eabcc784ec7c898e615f33e9db41e6a74e2f9
Nick Prough <nick.prough@gmail.com>
2018-05-07 09:12:05 -0400

    v1.12

--- api.py
+++ api.py
@@ -49,48 +49,48 @@ def update_item():
 
 @app.route('/shoppinglist/<item_id>', methods=['DELETE'])
 def delete_item(item_id):
-    mongo.db.shoppinglist.delete_one({'_id': ObjectId(item_id)})
+    mongo.db.shoppinglist.delete_one({'_id': ObjectId(task_id)})
     return jsonify({'deleted': True})
 
 # Task List
 @app.route('/tasklist', methods=['GET'])
-def get_items():
+def get_tasks():
     items = mongo.db.tasklist
     output = []
     for i in items.find():
         output.append({'name': i['name'],'_id':str(i['_id'])})
-    return jsonify({'items': output})
+    return jsonify({'tasks': output})
 
-@app.route('/tasklist/<item_id>', methods=['GET'])
-def get_item(item_id):
-    items = mongo.db.tasklist
+@app.route('/tasklist/<task_id>', methods=['GET'])
+def get_tasks(task_id):
+    tasks = mongo.db.tasklist
     output = ""
-    for i in items.find():
-        if str(i['_id']) == item_id:
+    for i in tasks.find():
+        if str(i['_id']) == task_id:
             output = i
     if (output == ""):
         abort(404)
-    return jsonify({'item': {'name': output['name'],'_id':str(output['_id'])}})
+    return jsonify({'task': {'name': output['name'],'_id':str(output['_id'])}})
 
 @app.route('/tasklist', methods=['POST'])
-def create_item():
+def create_task():
     if not request.json or not 'name' in request.json:
         abort(400)
-    newitem = {'name': request.json['name']}
-    mongo.db.tasklist.insert_one(newitem)
-    return jsonify({'new_item': {'name': request.json['name']}}), 201
+    newtask = {'name': request.json['name']}
+    mongo.db.tasklist.insert_one(newtask)
+    return jsonify({'new_task': {'name': request.json['name']}}), 201
 
 @app.route('/tasklist', methods=['PUT'])
-def update_item():
-    items = mongo.db.tasklist
+def update_task():
+    tasks = mongo.db.tasklist
     if not request.json or not 'name' in request.json:
         abort(400)
     mongo.db.tasklist.update_one({'_id': ObjectId(request.json['id'])},{"$set": {'name': request.json['name']}}, True)
-    return jsonify({'item': {'_id': request.json['id'], 'name': request.json['name']}})
+    return jsonify({'task': {'_id': request.json['id'], 'name': request.json['name']}})
 
-@app.route('/tasklist/<item_id>', methods=['DELETE'])
-def delete_item(item_id):
-    mongo.db.tasklist.delete_one({'_id': ObjectId(item_id)})
+@app.route('/tasklist/<task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    mongo.db.tasklist.delete_one({'_id': ObjectId(task_id)})
     return jsonify({'deleted': True})
 
 if __name__ == "__main__":
