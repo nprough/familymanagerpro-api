a5fe079cc5c2ea3f65e870a7b276e4d6eeaf73c1
Nick Prough <nick.prough@gmail.com>
2018-05-07 09:08:48 -0400

    v1.11

--- api.py
+++ api.py
@@ -11,6 +11,7 @@ app.config['MONGO_URI'] = os.environ.get('MONGO_URI', None)
 
 mongo = PyMongo(app)
 
+# Shopping List
 @app.route('/shoppinglist', methods=['GET'])
 def get_items():
     items = mongo.db.shoppinglist
@@ -49,6 +50,47 @@ def update_item():
 @app.route('/shoppinglist/<item_id>', methods=['DELETE'])
 def delete_item(item_id):
     mongo.db.shoppinglist.delete_one({'_id': ObjectId(item_id)})
+    return jsonify({'deleted': True})
+
+# Task List
+@app.route('/tasklist', methods=['GET'])
+def get_items():
+    items = mongo.db.tasklist
+    output = []
+    for i in items.find():
+        output.append({'name': i['name'],'_id':str(i['_id'])})
+    return jsonify({'items': output})
+
+@app.route('/tasklist/<item_id>', methods=['GET'])
+def get_item(item_id):
+    items = mongo.db.tasklist
+    output = ""
+    for i in items.find():
+        if str(i['_id']) == item_id:
+            output = i
+    if (output == ""):
+        abort(404)
+    return jsonify({'item': {'name': output['name'],'_id':str(output['_id'])}})
+
+@app.route('/tasklist', methods=['POST'])
+def create_item():
+    if not request.json or not 'name' in request.json:
+        abort(400)
+    newitem = {'name': request.json['name']}
+    mongo.db.tasklist.insert_one(newitem)
+    return jsonify({'new_item': {'name': request.json['name']}}), 201
+
+@app.route('/tasklist', methods=['PUT'])
+def update_item():
+    items = mongo.db.tasklist
+    if not request.json or not 'name' in request.json:
+        abort(400)
+    mongo.db.tasklist.update_one({'_id': ObjectId(request.json['id'])},{"$set": {'name': request.json['name']}}, True)
+    return jsonify({'item': {'_id': request.json['id'], 'name': request.json['name']}})
+
+@app.route('/tasklist/<item_id>', methods=['DELETE'])
+def delete_item(item_id):
+    mongo.db.tasklist.delete_one({'_id': ObjectId(item_id)})
     return jsonify({'deleted': True})
 
 if __name__ == "__main__":
